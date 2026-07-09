f91767c3bafe92ba505a4f3a5161f9f3fc482ba5
srikantha1812 <727725eucd603@skcet.ac.in>
2026-07-09 14:19:42 +0530

    chore: initial commit [backend]

---
+++ backend/FTApp/src/main/java/com/example/FTApp/FtAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.FTApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FtAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FtAppApplication.class, args);
+	}
+
+}
---
+++ backend/FTApp/src/test/java/com/example/FTApp/FtAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.FTApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FtAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
